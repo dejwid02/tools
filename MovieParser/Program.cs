9aa60c65a925f7ede4da9cce7497ec1f418b2a23
dejwid02 <dejwid02@wp.pl>
2020-01-05 23:14:24 +0100

    parser

---
+++ MovieParser/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Web;
+using System.Linq;
+using System.Net;
+using System.IO;
+namespace MovieParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var url = args[0];
+            var content = client.DownloadString(url);
+            client.Dispose();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(content);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
