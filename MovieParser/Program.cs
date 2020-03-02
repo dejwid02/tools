3b75bdf0a17e181446421d838fe0c8484abbdb76
dejwid02 <dejwid02@wp.pl>
2020-03-02 22:08:27 +0100

    new parser

--- MovieParser/Program.cs
+++ MovieParser/Program.cs
@@ -11,6 +11,9 @@ using MovieParser.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.FileExtensions;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace MovieParser
 {
     class Program
@@ -38,11 +41,8 @@ namespace MovieParser
 
                 var channels = repository.GetAllChannels();
                 var filterDate = repository.GetLastLog()?.LastSynchronizedDate ?? DateTime.Now;
-                var scheduleParser = new EpgParser();
-                Console.WriteLine("Downloading data...");
-                var webClient = new WebClient();
-                var content = webClient.DownloadString(providerUrl);
-                webClient.Dispose();
+                EpgParser scheduleParser = new EpgParser();
+                string content= GetTeleContent(providerUrl).Result;
                 Console.WriteLine("Parsing...");
                 var tvItems = scheduleParser.ParseTvSchedule(channels, content).ToList();
 
@@ -59,12 +59,12 @@ namespace MovieParser
                     else
                     {
                         existingMovies.Add(tvListingItem.Movie);
-                        for (int i =0; i< tvListingItem.Movie.Actors.Count; i++)
+                        for (int i = 0; i < tvListingItem.Movie.Actors.Count; i++)
                         {
                             var actor = tvListingItem.Movie.Actors[i];
                             var existingActor = existingActors.FirstOrDefault(a => a.FirstName == actor.FirstName && a.LastName == actor.LastName)
                                 ?? repository.GetActorsByName(actor.FirstName, actor.LastName);
-                            if (existingActor!= null)
+                            if (existingActor != null)
                             {
                                 tvListingItem.Movie.Actors[i] = existingActor;
                             }
@@ -112,6 +112,39 @@ namespace MovieParser
             };
             repository.Add(configurationItem);
             repository.SaveChanges();
+        }
+        private async static Task<string> GetTeleContent(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), url + "filmy"))
+                {
+                    request.Headers.TryAddWithoutValidation("authority", url);
+                    request.Headers.TryAddWithoutValidation("cache-control", "max-age=0");
+                    request.Headers.TryAddWithoutValidation("upgrade-insecure-requests", "1");
+                    request.Headers.TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
+                    request.Headers.TryAddWithoutValidation("sec-fetch-dest", "document");
+                    request.Headers.TryAddWithoutValidation("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                    request.Headers.TryAddWithoutValidation("sec-fetch-site", "same-origin");
+                    request.Headers.TryAddWithoutValidation("sec-fetch-mode", "navigate");
+                    request.Headers.TryAddWithoutValidation("sec-fetch-user", "?1");
+                    request.Headers.TryAddWithoutValidation("referer", url + "moje-stacje");
+                    request.Headers.TryAddWithoutValidation("accept-language", "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7");
+                    request.Headers.TryAddWithoutValidation("cookie", "_ga=GA1.2.550524766.1583165680; _gid=GA1.2.514516976.1583165680; __gads=ID=aa00795e9f054f80:T=1583165683:S=ALNI_MbB_FabtWs_bc1YKqUtPpiSgC5sww; SID=ge2nbc6lm4j9mbw4bva7ud01uztfp8ur");
+
+                    var response = await httpClient.SendAsync(request);
+                    var result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
+            }
+        }
+        private static string GetContent(string providerUrl)
+        {
+            Console.WriteLine("Downloading data...");
+            var webClient = new WebClient();
+            var content = webClient.DownloadString(providerUrl);
+            webClient.Dispose();
+            return content;
         }
     }
 }
