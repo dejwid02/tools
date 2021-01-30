1f647461dc0fc8cb13ce8f407cf55b3e17c2d08b
dejwid02 <dejwid02@wp.pl>
2021-01-30 22:42:27 +0100

    start filtering

--- MoviesManagement/MoviesManagement/wwwroot/js/site.js
+++ MoviesManagement/MoviesManagement/wwwroot/js/site.js
@@ -4,3 +4,26 @@
     //    //alert(e.target.children("option:selected").val());
     //});
 });
+
+function filter() {
+   
+
+    var movies = document.getElementsByClassName("movie");
+    Array.prototype.forEach.call(movies, function (el) {filterItem(el)});
+
+
+}
+
+function filterItem(element) {
+    const input = document.getElementsByClassName("search-input").item(0);
+    const text = input.value;
+
+    const title = element.getElementsByClassName("card-title")[0].innerText;
+    if (!title.includes(text) && text.length > 3) {
+        element.classList.add("d-none");
+    }
+    else {
+        element.classList.remove("d-none");
+    }
+
+}
